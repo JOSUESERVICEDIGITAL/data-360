70c0c7b5522e783282392f8eb82e9922c9c0e238
josueservicedigital <pawendtaoreo92@gmail.com>
2026-08-16 11:27:48 +0100

    json corriger

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\User;
-use Faker\Generator;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -25,11 +24,9 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $faker = app(Generator::class);
-
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
